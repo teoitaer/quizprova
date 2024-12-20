07689b4fd09e870bc70cdb856b2c53001c16ac57
teoitaer <129729534+teoitaer@users.noreply.github.com>
2024-12-20 11:17:11 +0100

    Update end.js

--- end.js
+++ end.js
@@ -12,29 +12,24 @@ const studentSurname = localStorage.getItem('surname');
 document.getElementById('subjectName').innerText = selectedSubject;
 document.getElementById('studentName').innerText = `${studentName} ${studentSurname}`;
 
-
-//const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
-
-//const MAX_HIGH_SCORES = 10;
-
 // Aggiungi il simbolo di percentuale al punteggio finale
 finalScore.innerText = mostRecentScore + '%';
 
-username.addEventListener('keyup', () => {
-    saveScoreBtn.disabled = !username.value;
-});
+// username.addEventListener('keyup', () => {
+  //   saveScoreBtn.disabled = !username.value;
+// });
 
-saveHighScore = (e) => {
-    e.preventDefault();
+// saveHighScore = (e) => {
+   //  e.preventDefault();
 
-    const score = {
-        score: mostRecentScore,
-        name: username.value,
-    };
-    highScores.push(score);
-    highScores.sort((a, b) => b.score - a.score);
-    highScores.splice(5);
+    // const score = {
+       //  score: mostRecentScore,
+        // name: username.value,
+//     };
+//     highScores.push(score);
+//     highScores.sort((a, b) => b.score - a.score);
+//     highScores.splice(5);
 
-    localStorage.setItem('highScores', JSON.stringify(highScores));
-    window.location.assign('/');
-};
+//     localStorage.setItem('highScores', JSON.stringify(highScores));
+//     window.location.assign('/');
+// };
